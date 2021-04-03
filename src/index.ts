216de2dc00bcac2b9255f196f9ffbe6e087cd93a
Hannes Wörn <hannes@woern.de>
2021-04-03 15:08:47 +0200

    Completed brick layouting

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import * as screen from "./lib/screen";
 
 // Settings
 const canvasSize: screen.Rectangle = {
-    width: 720,
-    height: 500,
+    width: 800,
+    height: 625,
     margins: {
         x: 20,
-        y: 40,
+        y: 25,
     },
 };
 
@@ -14,8 +14,17 @@ const brickSize: screen.Rectangle = {
     width: 50,
     height: 50,
     margins: {
-        x: 25,
-        y: 25,
+        x: 10,
+        y: 10,
+    },
+};
+
+const paddleSize: screen.Rectangle = {
+    width: 100,
+    height: 25,
+    margins: {
+        x: 0,
+        y: 0,
     },
 };
 
@@ -25,36 +34,58 @@ canvas.setAttribute("width", String(canvasSize.width));
 canvas.setAttribute("height", String(canvasSize.height));
 let ctx = canvas.getContext("2d");
 
+// globals
+let ballPosition: number[];
+
 function initialize(ctx: CanvasRenderingContext2D) {
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Calculate brick amount based on widths
-    const brickColumnCount = screen.calcBoxAmount(
-        canvasSize,
-        brickSize,
-        screen.Axis.X
-    );
-    const brickRowCount = screen.calcBoxAmount(
-        canvasSize,
-        brickSize,
-        screen.Axis.Y
-    );
-    // Draw the bricks on screen
-    const tickX =
-        (canvasSize.width - 2 * canvasSize.margins.x) / (brickColumnCount + 1);
-    const tickY =
-        (canvasSize.height - 2 * canvasSize.margins.y) / (brickRowCount + 1);
-    let cursor: number[] = [canvasSize.margins.x, canvasSize.margins.y];
 
-    for (let i = 0; i < brickRowCount; i++) {
-        for (let j = 0; j < brickColumnCount; j++) {
-            ctx.beginPath();
-            ctx.rect(cursor[0] + j * tickY, cursor[1] + i * tickX, 50, 50);
-            ctx.fillStyle = "#FF0000";
-            ctx.fill();
-            ctx.closePath();
+    // draw bricks
+    const brickCoords = screen.calcBrickOrigins(canvasSize, brickSize);
+    let maxY = 0; // needed later for ball drawing
+    for (let i = 0; i < brickCoords.length; i++) {
+        ctx.beginPath();
+        ctx.rect(
+            brickCoords[i][0],
+            brickCoords[i][1],
+            brickSize.width,
+            brickSize.height
+        );
+        ctx.fillStyle = "#90ee90";
+        ctx.fill();
+        ctx.closePath();
+        // keep track of max y for drawing the ball later on
+        if (brickCoords[i][1] > maxY) {
+            maxY = brickCoords[i][1];
         }
     }
+
+    // draw paddle
+    ctx.beginPath();
+    ctx.rect(
+        canvasSize.width / 2 - paddleSize.width / 2,
+        canvasSize.height - paddleSize.height * 2,
+        paddleSize.width,
+        paddleSize.height
+    );
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+
+    // draw ball
+    ctx.beginPath();
+    const radius = 20;
+    const ballX = canvasSize.width / 2;
+    const ballY = maxY + brickSize.height + 2 * radius;
+    ctx.arc(ballX, ballY, radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+    ballPosition = [ballX, ballY];
 }
 
+function gameLoop() {}
+
 initialize(ctx);
+setInterval(gameLoop, 10);
--- src/lib/screen.ts
+++ src/lib/screen.ts
@@ -12,31 +12,48 @@ export enum Axis {
     Y,
 }
 
-export function calcBoxAmount(
+export function calcBrickOrigins(
     canvasSize: Rectangle,
-    box: Rectangle,
-    axis: Axis = Axis.X
-): number {
-    let availableSpace: number;
-    let boxSize: number;
-    let gap: number;
-    if (axis == Axis.X) {
-        availableSpace = canvasSize.width - 2 * canvasSize.margins.x;
-        gap = box.margins.x;
-        boxSize = box.width + gap;
-    } else if (axis == Axis.Y) {
-        availableSpace = canvasSize.height - 2 * canvasSize.margins.y;
-        gap = box.margins.y;
-        boxSize = box.height + gap;
-    }
-    let amount = Math.floor(availableSpace / boxSize);
-    let remainer = availableSpace % boxSize;
+    box: Rectangle
+): number[][] {
+    const availableWidth = canvasSize.width - 2 * canvasSize.margins.x;
+    const availableHeight = canvasSize.height - 8 * canvasSize.margins.y; // Add more margin to the bottom
+    let gapX = box.margins.x;
+    let gapY = box.margins.y;
 
-    /*
-    if (remainer < gap) {
-        amount = amount - 1;
+    // calculate the amount of columns
+    const fractionColumns = availableWidth / (box.width + gapX);
+    let columns = Math.floor(fractionColumns);
+    let remainerColumns = fractionColumns - columns;
+    // Check if there is enough space left to add another gap
+    // e.g. for 5 bricks we need 6 gaps. If not enough space is left - remove one column
+    if (remainerColumns < gapX) {
+        columns = columns - 1;
     }
-    */
+    gapX = (availableWidth - columns * box.width) / (columns + 1);
 
-    return amount;
+    // Do the same thing for rows
+    const fractionRows = availableHeight / (box.width + gapY);
+    let rows = Math.floor(fractionRows);
+    let remainerRows = fractionRows - rows;
+    // Check if there is enough space left to add another gap
+    // e.g. for 5 bricks we need 6 gaps. If not enough space is left - remove one column
+    if (remainerRows < gapY) {
+        rows = rows - 1;
+    }
+    gapY = (availableHeight - rows * box.height) / (rows + 1);
+
+    let origins: number[][] = [];
+    let x = gapX + canvasSize.margins.x;
+    let y = gapY + canvasSize.margins.y;
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows; r++) {
+            origins.push([
+                x + c * (box.width + gapX),
+                y + r * (box.height + gapY),
+            ]);
+        }
+    }
+
+    return origins;
 }
